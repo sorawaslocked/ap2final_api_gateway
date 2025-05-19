19633a383480a2a26898ae48af767ad6f92bfee6
sora <133684349+sorawaslocked@users.noreply.github.com>
2025-05-19 07:39:57 +0500

    Add movie model

---
+++ internal/model/movie.go
@@ -0,0 +1,16 @@
+package model
+
+import "time"
+
+type Movie struct {
+	ID               string
+	AgeRating        string
+	PrimaryTitle     string
+	OriginalTitle    string
+	ReleaseYear      uint16
+	RuntimeInMinutes uint16
+	Genres           []string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	IsDeleted        bool
+}
